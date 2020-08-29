e527f8e35d36e1d46fc0bf93c2c4933aee183c32
kingo123123 <69599600+kingo123123@users.noreply.github.com>
2020-08-29 04:23:13 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,11 @@
+module.exports = {
+
+    TOKEN: 'NzQ2Njk3MTEwOTA0MjQyMjA5.X0EF4A.I1Pw2mQQL_5Bgcn2-Ca2rw2MCOw',    
+
+    YT_API_KEY: 'api ', 
+
+    prefix: '=',
+
+    devs: ['722915852344885299']
+
+}
